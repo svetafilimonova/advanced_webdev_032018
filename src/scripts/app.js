a42189d5fc156cb33276a236bbd88e41199c7276
Svetlana Filimonova <lana.shvedova@gmail.com>
2018-04-09 08:50:26 +0300

    intro validation wrapped in module and it does not work

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -66,7 +66,7 @@ if($('.comment__form--works').length) {
 
 
 if($('.auth-form').length) {
-    introForm();
+    introForm.init();
 }
 
 
--- src/scripts/common/intro-validation.js
+++ src/scripts/common/intro-validation.js
@@ -1,46 +1,56 @@
-const introForm = document.querySelector('.auth-form');
-const login = document.getElementById('login');
-const password = document.getElementById('password');
-const submitBtn = document.querySelector('.auth-form__button-login');
-const parent = document.querySelector('.auth-form__row');
+const introValidation = (function () {
 
+  return {
 
+    init: function () {
 
-    function showError(elem, container, errorMessage) {
-      elem.classList.add("error");
-      var msgElem = document.createElement('div');
-      msgElem.className = "comment__form-error";
-      msgElem.innerHTML = errorMessage;
-      container.appendChild(msgElem);
+      const introForm = document.querySelector('.auth-form');
+      const login = document.getElementById('login');
+      const password = document.getElementById('password');
+      const submitBtn = document.querySelector('.auth-form__button-login');
+      const parent = document.querySelector('.auth-form__row');
+      
+      
+      
+          function showError(elem, container, errorMessage) {
+            elem.classList.add("error");
+            var msgElem = document.createElement('div');
+            msgElem.className = "comment__form-error";
+            msgElem.innerHTML = errorMessage;
+            container.appendChild(msgElem);
+          }
+      
+          function resetError(elem, container) {
+            elem.classList.remove("error");
+            if (container.lastChild.className == "comment__form-error") {
+              container.removeChild(container.lastChild);
+            }
+          }
+      
+      
+            function introValidation () {
+              introForm.addEventListener('submit', function(e){
+       
+            resetError(login, login.closest(".auth-form__row"));
+            if (!login.value) {
+              e.preventDefault();
+              showError(login, login.closest(".auth-form__row"), 'Введите логин');
+            }
+      
+            resetError(password, password.closest(".auth-form__row"));
+            if (!password.value) {
+              e.preventDefault();
+              showError(password, password.closest(".auth-form__row"), 'Введите пароль');
+            }
+      
+      
+      
+          }, false);
+        };
     }
-
-    function resetError(elem, container) {
-      elem.classList.remove("error");
-      if (container.lastChild.className == "comment__form-error") {
-        container.removeChild(container.lastChild);
-      }
-    }
-
-
-      function introValidation () {
-        introForm.addEventListener('submit', function(e){
- 
-      resetError(login, login.closest(".auth-form__row"));
-      if (!login.value) {
-        e.preventDefault();
-        showError(login, login.closest(".auth-form__row"), 'Введите логин');
-      }
-
-      resetError(password, password.closest(".auth-form__row"));
-      if (!password.value) {
-        e.preventDefault();
-        showError(password, password.closest(".auth-form__row"), 'Введите пароль');
-      }
-
-
-
-    }, false);
-  };
+  }
   
+}());
+
 
 module.exports = introValidation;
